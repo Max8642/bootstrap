861be6962b4c10a45e4b6da283e81c4b25a51d91
Max8642 <memasri101@gmail.com>
2024-03-26 10:58:47 -0400

    Bootstrap with index.py

--- startbootstrap-simple-sidebar-gh-pages/index.py
+++ startbootstrap-simple-sidebar-gh-pages/index.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    #return render_template('index.html')  # this line only displays files in the templates sub directory.  To display from other directories, see below
-   return flask.send_from_directory(".", path="index.html")
+   return flask.send_from_directory(".", path="Templates/index.html")
 
 
 if __name__ == '__main__':
